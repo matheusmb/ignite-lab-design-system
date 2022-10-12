4b2cc821289cf45decfefc224c08d5f83038556d
matheusmb <matheusmb@outlook.com>
2022-10-12 17:58:45 -0300

    docs: add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
+import "./styles/global.css";
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600 transition-all">
+        Enviar
+      </button>
+    </>
+  );
 }
